42fe01c5ebd2164624639596d691c52b8980085b
pipinkurniawati <13513089@std.stei.itb.ac.id>
2016-04-24 17:38:02 +0700

    function load textures

--- Latihan03a/rubik.cpp
+++ Latihan03a/rubik.cpp
@@ -18,8 +18,6 @@ unsigned int imageSize;   // = width*height*3
 // Actual RGB data
 unsigned char * data;
 
-GLuint image = loadBMP_custom("./my_texture.bmp");
-
 double rotate_x = 0;
 double rotate_y = 0;
 double rotate_z = 0;
@@ -31,6 +29,8 @@ typedef struct {
 
     float x, y, z; // menentukan vektor rotasi
 } sc;
+
+GLuint texture;
 
 sc matrix_cube[3][3][3];
 
@@ -44,6 +44,57 @@ void init() {
             }
         }
     }
+}
+
+GLuint LoadTextureRAW( const char * filename, int wrap )
+{
+    int width, height;
+    BYTE * data;
+    FILE * file;
+
+    // open texture data
+    file = fopen( filename, "rb" );
+    if ( file == NULL ) return 0;
+
+    // allocate buffer
+    width = 256;
+    height = 256;
+    data = (BYTE*) malloc( width * height * 3 );
+
+    // read texture data
+    fread( data, width * height * 3, 1, file );
+    fclose( file );
+
+    // allocate a texture name
+    glGenTextures( 1, &texture );
+
+    // select our current texture
+    glBindTexture( GL_TEXTURE_2D, texture );
+
+    // select modulate to mix texture with color for shading
+    glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
+
+    // when texture area is small, bilinear filter the closest mipmap
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                     GL_LINEAR_MIPMAP_NEAREST );
+    // when texture area is large, bilinear filter the first mipmap
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+    // if wrap is true, the texture wraps over at the edges (repeat)
+    //       ... false, the texture ends at the edges (clamp)
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                     wrap ? GL_REPEAT : GL_CLAMP );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
+                     wrap ? GL_REPEAT : GL_CLAMP );
+
+    // build our texture mipmaps
+    gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height,
+                       GL_RGB, GL_UNSIGNED_BYTE, data );
+
+    // free buffer
+    free( data );
+
+    return texture;
 }
 
 void controls(GLFWwindow* window, int key, int scancode, int action, int mods)
